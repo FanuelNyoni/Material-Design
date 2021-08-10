03a0cb22778a7285e480c503d4bf79f7291d8794
Fanuel Nyoni <fancyee10@gmail.com>
2021-08-10 14:22:11 +0200

    yes

--- src/styles.js
+++ src/styles.js
@@ -1,10 +1,10 @@
-import { makeStyles } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
     button: {
         color: "white",
         background: "black"
-    }
-})
+    },
+});
 
 export default useStyles;
