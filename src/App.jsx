79ebe6ce5b602b17942eae168698b971202b59a9
Fanuel Nyoni <fancyee10@gmail.com>
2021-08-10 20:00:11 +0200

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Grid } from "@material-ui/core";
 import Navbar from "./components/navbar/Navbar.jsx";
-import   useStyles  from "./styles.js";
+import useStyles from "./styles.js";
 
 function App() {
   const classes = useStyles();
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <Grid container>
+        <Grid item sm></Grid>
+<Grid item sm={2}>
+  <Leftbar />
+</Grid>
+<Grid item sm={7}></Grid>
+<Grid item sm={12}></Grid>
+      </Grid>
+    </div>
   );
 }
 
