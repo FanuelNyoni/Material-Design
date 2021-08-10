afd46fdfbaffc8d62b5a34fa73f49e771753105c
Fanuel Nyoni <fancyee10@gmail.com>
2021-08-10 19:10:10 +0200

    yes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import useStyles from './styles';
+import React, { useState } from "react";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { Mail, Notifications, Search, Settings } from "@material-ui/icons";
+import useStyles from "./styles";
 
 const Navbar = () => {
-    const classes = useStyles();
-    return (
-        <AppBar>
-            <Toolbar>
-                <Typography variant="h6" className={classes.logoLg}>
-                    webSpot dev
-                </Typography>
-                <Typography variant="h6" className={classes.logoSm}>
-                    webSpot
-                </Typography>
-                <div className={classes.search}>
-                    <Search /> 
-                </div>
-            </Toolbar>
-        </AppBar>
-    );
+    const [open, setOpen] = useState(false)
+
+  const classes = useStyles({open});
+  return (
+    <AppBar className={classes.appbar}>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6" className={classes.logoLg}>
+          webSpot dev
+        </Typography>
+        <Typography variant="h6" className={classes.logoSm}>
+          webSpot
+        </Typography>
+        <div className={classes.search}>
+          <Search className={classes.searchIcon} />
+          <InputBase placeholder="search..." className={classes.Inputbase} />
+        </div>
+        <div className={classes.settings}>
+            <Search className={classes.setSearchIcon} onClick={()=> setOpen(true)}/>
+          <Badge className={classes.badge} variant="dot" color="primary">
+            <Mail />
+          </Badge>
+          <Badge className={classes.badge} variant="dot" color="primary">
+            <Notifications />
+          </Badge>
+          <Settings className={classes.settingsIcon}/>
+          <Avatar alt="Cindy Baker" src="./images/avatar.png" />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
-export default Navbar
+export default Navbar;
