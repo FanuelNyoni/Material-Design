d95ce23e4f0bb12f61da27a6fa7a011828ebe176
Fanuel Nyoni <fancyee10@gmail.com>
2021-08-10 15:32:25 +0200

    yes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import useStyles from '../styles';
+import useStyles from '../../styles';
 
 const Navbar = () => {
     const classes = useStyles();
     return (
         <AppBar>
             <Toolbar>
-                <Typography variant="h6">
-                    WebSpot dev
+                <Typography variant="h6" className={classes.logoLg}>
+                    webSpot dev
+                </Typography>
+                <Typography variant="h6" className={classes.logoLg}>
+                    webSpot
                 </Typography>
             </Toolbar>
         </AppBar>
