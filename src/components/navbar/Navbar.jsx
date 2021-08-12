b6ee59b763ffcba1cdea6e2396a210b0fddbd13e
Fanuel Nyoni <fancyee10@gmail.com>
2021-08-13 01:06:20 +0200

    yes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,59 +1,59 @@
 import React, { useState } from "react";
 import {
-    AppBar,
-    Avatar,
-    Badge,
-    InputBase,
-    Toolbar,
-    Typography,
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  Toolbar,
+  Typography,
 } from "@material-ui/core";
 import {
-    Cancel,
-    Mail,
-    Notifications,
-    Search,
-    Settings,
+  Cancel,
+  Mail,
+  Notifications,
+  Search,
+  Settings,
 } from "@material-ui/icons";
 import useStyles from "./styles";
 
 const Navbar = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const classes = useStyles({ open });
-    return (
-        <AppBar className={classes.appbar}>
-            <Toolbar className={classes.toolbar}>
-                <Typography variant="h6" className={classes.logoLg}>
-                    webSpot dev
-                </Typography>
-                <Typography variant="h6" className={classes.logoSm}>
-                    webSpot
-                </Typography>
-                <div className={classes.search}>
-                    <Search className={classes.searchIcon} />
-                    <InputBase placeholder="search..." className={classes.Inputbase} />
-                    <Cancel
-                        className={classes.cancel}
-                        onClick={() => setOpen(false)}
-                    ></Cancel>
-                </div>
-                <div className={classes.settings}>
-                    <Search
-                        className={classes.setSearchIcon}
-                        onClick={() => setOpen(true)}
-                    />
-                    <Badge className={classes.badge} variant="dot" color="primary">
-                        <Mail />
-                    </Badge>
-                    <Badge className={classes.badge} variant="dot" color="primary">
-                        <Notifications />
-                    </Badge>
-                    <Settings className={classes.settingsIcon} />
-                    <Avatar alt="Cindy Baker" src="./images/avatar.png" />
-                </div>
-            </Toolbar>
-        </AppBar>
-    );
+  const classes = useStyles({ open });
+  return (
+    <AppBar className={classes.appbar}>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6" className={classes.logoLg}>
+          . . . Jus4Dev
+        </Typography>
+        <Typography variant="h6" className={classes.logoSm}>
+          Jus4Dev
+        </Typography>
+        <div className={classes.search}>
+          <Search className={classes.searchIcon} />
+          <InputBase placeholder="search..." className={classes.Inputbase} />
+          <Cancel
+            className={classes.cancel}
+            onClick={() => setOpen(false)}
+          ></Cancel>
+        </div>
+        <div className={classes.settings}>
+          <Search
+            className={classes.setSearchIcon}
+            onClick={() => setOpen(true)}
+          />
+          <Badge className={classes.badge} variant="dot" color="primary">
+            <Mail />
+          </Badge>
+          <Badge className={classes.badge} variant="dot" color="primary">
+            <Notifications />
+          </Badge>
+          <Settings className={classes.settingsIcon} />
+          <Avatar alt="Cindy Baker" src="./images/avatar.png" />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,13 +4,11 @@ import {
     Container,
     ImageList,
     ImageListItem,
-    makeStyles,
     Typography,
     Divider,
   } from "@material-ui/core";
   import { AvatarGroup } from "@material-ui/lab";
-  
-
+import useStyles from "./styles";
   
   const Rightbar = () => {
     const classes = useStyles();
