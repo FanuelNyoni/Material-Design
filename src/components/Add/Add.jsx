3b51aeb8c934cc2abd6bee3609ad959ebb7e92d1
Fanuel Nyoni <fancyee10@gmail.com>
2021-08-12 19:30:29 +0200

    yes

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -1,16 +1,24 @@
-import { Fab, Tooltip } from "@material-ui/core";
+import { Fab, Modal, Tooltip } from "@material-ui/core";
 import { Add as AddIcon } from "@material-ui/icons";
+import { useState } from "react";
 
 import useStyles from "./styles";
 
 const Add = () => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+
   return (
-    <Tooltip title="Add" aria-label="add">
-      <Fab color="secondary" className={classes.absolute}>
+      <>
+    <Tooltip title="Add" aria-label="add" onClick={()=> setOpen(true)}>
+      <Fab color="secondary" className={classes.fab}>
         <AddIcon />
       </Fab>
     </Tooltip>
+    <Modal open={open}>Hello User</Modal>
+
+    </>
   );
 };
 
