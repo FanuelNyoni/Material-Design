5a6ae9d1c7c492d8c787ac63a2ab8a247bcd297f
Fanuel Nyoni <fancyee10@gmail.com>
2021-08-12 19:57:14 +0200

    yes

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -1,4 +1,4 @@
-import { Fab, Modal, Tooltip } from "@material-ui/core";
+import { Container, Fab, Modal, TextField, Tooltip } from "@material-ui/core";
 import { Add as AddIcon } from "@material-ui/icons";
 import { useState } from "react";
 
@@ -7,17 +7,87 @@ import useStyles from "./styles";
 const Add = () => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
 
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenAlert(false);
+  };
 
   return (
-      <>
-    <Tooltip title="Add" aria-label="add" onClick={()=> setOpen(true)}>
-      <Fab color="secondary" className={classes.fab}>
-        <AddIcon />
-      </Fab>
-    </Tooltip>
-    <Modal open={open}>Hello User</Modal>
-
+    <>
+      <Tooltip title="Add" aria-label="add" onClick={() => setOpen(true)}>
+        <Fab color="secondary" className={classes.fab}>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <Container className={classes.container}>
+        <form className={classes.form} autoComplete="off">
+        <div className={classes.item}>
+              <TextField
+                id="standard-basic"
+                label="Title"
+                size="small"
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField
+                id="outlined-multiline-static"
+                multiline
+                rows={4}
+                defaultValue=""
+                variant="outlined"
+                label="Description"
+                size="small"
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField select label="Visibility" value="Public">
+                <MenuItem value="Public">Public</MenuItem>
+                <MenuItem value="Private">Private</MenuItem>
+                <MenuItem value="Unlisted">Unlisted</MenuItem>
+              </TextField>
+            </div>
+            <div className={classes.item}>
+              <FormLabel component="legend">Who can comment?</FormLabel>
+              <RadioGroup>
+                <FormControlLabel
+                  value="Everybody"
+                  control={<Radio size="small" />}
+                  label="Everybody"
+                />
+                <FormControlLabel
+                  value="My Friends"
+                  control={<Radio size="small" />}
+                  label="My Friends"
+                />
+                <FormControlLabel
+                  value="Nobody"
+                  control={<Radio size="small" />}
+                  label="Nobody"
+                />
+                <FormControlLabel
+                  value="Custom"
+                  disabled
+                  control={<Radio size="small" />}
+                  label="Custom (Premium)"
+                />
+              </RadioGroup>
+            </div>
+        </form>
+        </Container>
+      </Modal>
     </>
   );
 };
